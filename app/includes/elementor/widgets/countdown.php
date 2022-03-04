658e31bd5d156db49cfabc91a0e912a0c60872cc
rejuancse <rejuancse@gmail.com>
2022-03-04 11:30:44 +0600

    Count down addons bug fixed

--- app/includes/elementor/widgets/countdown.php
+++ app/includes/elementor/widgets/countdown.php
@@ -28,15 +28,15 @@ class Widget_CountDown extends Widget_Base {
             ]
         );
 
-		# Title text
+		# Countdown intro
 		$this->add_control(
-            'title_text',
+            'countdown_intro',
             [
-                'label' => __( 'Title Text', 'wpew' ),
+                'label' => __( 'CountDown Intro', 'wpew' ),
                 'type' => Controls_Manager::TEXT,
                 'label_block' => true,
-                'placeholder' => __( 'Enter hotline', 'wpew' ),
-                'default' => __( 'Deal of the Day', 'wpew' ),
+                'placeholder' => __( 'Enter intro text', 'wpew' ),
+                'default' => __( 'Ends In', 'wpew' ),
             ]
         );
 
@@ -51,17 +51,46 @@ class Widget_CountDown extends Widget_Base {
             ]
         );
 
+		$this->add_responsive_control(
+			'align',
+			[
+				'label' => esc_html__( 'Alignment', 'wpew' ),
+				'type' => Controls_Manager::CHOOSE,
+				'options' => [
+					'left' => [
+						'title' => esc_html__( 'Left', 'wpew' ),
+						'icon' => 'eicon-text-align-left',
+					],
+					'center' => [
+						'title' => esc_html__( 'Center', 'wpew' ),
+						'icon' => 'eicon-text-align-center',
+					],
+					'right' => [
+						'title' => esc_html__( 'Right', 'wpew' ),
+						'icon' => 'eicon-text-align-right',
+					],
+					'justify' => [
+						'title' => esc_html__( 'Justified', 'wpew' ),
+						'icon' => 'eicon-text-align-justify',
+					],
+				],
+				'default' => '',
+				'selectors' => [
+					'{{WRAPPER}}' => 'text-align: {{VALUE}};',
+				],
+			]
+		);
+
         $this->end_controls_section();
         # Option End
 
 		$this->start_controls_section(
 			'section_title_style',
 			[
-				'label' 	=> __( 'Title', 'wpew' ),
+				'label' 	=> __( 'CountDown Title', 'wpew' ),
 				'tab' 		=> Controls_Manager::TAB_STYLE,
 			]
 		);
-
 
 		# Title text color
 		$this->add_control(
@@ -70,19 +99,99 @@ class Widget_CountDown extends Widget_Base {
 				'label'		=> __( 'Title Text Color', 'wpew' ),
 				'type'		=> Controls_Manager::COLOR,
 				'selectors' => [
-					'{{WRAPPER}} .main-title h2' => 'color: {{VALUE}};',
+					'{{WRAPPER}} .countdown-wrap .deal_counter .list-inline-item' => 'color: {{VALUE}};',
+					'{{WRAPPER}} .countdown-wrap .deal_counter .list-inline-item span.seperator' => 'color: {{VALUE}};',
 				],
 			]
 		);
 
+		# Title text color
+		$this->add_control(
+			'countdown_bg_color',
+			[
+				'label'		=> __( 'Background Color', 'wpew' ),
+				'type'		=> Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .countdown-wrap .deal_counter.bgc-thm' => 'background-color: {{VALUE}};',
+				],
+			]
+		);
 
 		# Title text typography
 		$this->add_group_control(
 			Group_Control_Typography::get_type(),
 			[
-				'label'		=> __( 'Title Text Typography', 'wpew' ),
-				'name' 		=> 'title_typography',
-				'selector' 	=> '{{WRAPPER}} .main-title h2',
+				'label'		=> __( 'CountDown Typography', 'wpew' ),
+				'name' 		=> 'countdown_typography',
+				'selector' 	=> '{{WRAPPER}} .countdown-wrap .deal_counter .list-inline-item',
+			]
+		);
+
+		# Padding
+		$this->add_responsive_control(
+            'countdown_padding',
+            [
+                'label' 		=> __( 'Padding', 'wpew' ),
+                'type' 			=> Controls_Manager::DIMENSIONS,
+                'size_units' 	=> [ 'px', 'em', '%' ],
+                'selectors' 	=> [
+                    '{{WRAPPER}} .countdown-wrap .deal_counter' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+                ],
+                'separator' 	=> 'before',
+            ]
+        );
+
+		/**
+		 * Intro Text
+		 */
+		$this->add_control(
+			'timeline_intro_section',
+			[
+				'label' => esc_html__( 'Intro Text Style', 'wpew' ),
+				'type' => Controls_Manager::HEADING,
+				'separator' => 'before',
+			]
+		);
+
+		# Title text color
+		$this->add_control(
+			'intro_text_color',
+			[
+				'label'		=> __( 'Intro Text Color', 'wpew' ),
+				'type'		=> Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .countdown-wrap .deal_counter .list-inline-item .title' => 'color: {{VALUE}};',
+				],
+			]
+		);
+
+		# Title text typography
+		$this->add_group_control(
+			Group_Control_Typography::get_type(),
+			[
+				'label'		=> __( 'Intro Text Typography', 'wpew' ),
+				'name' 		=> 'intro_typography',
+				'selector' 	=> '{{WRAPPER}} .countdown-wrap .deal_counter .list-inline-item .title',
+			]
+		);
+
+		$this->add_responsive_control(
+			'intro_text_space',
+			[
+				'label' => esc_html__( 'Spacing', 'wpew' ),
+				'type' => Controls_Manager::SLIDER,
+				'default' => [
+					'size' => 20,
+				],
+				'range' => [
+					'px' => [
+						'min' => 0,
+						'max' => 100,
+					],
+				],
+				'selectors' => [
+					'{{WRAPPER}} .countdown-wrap .deal_counter .list-inline-item .title' => 'margin-right: {{SIZE}}{{UNIT}};',
+				],
 			]
 		);
 
@@ -93,21 +202,26 @@ class Widget_CountDown extends Widget_Base {
 
 	protected function render( ) {
 		$settings = $this->get_settings();
-		$title_text = $settings['title_text'];
+		$countdown_intro = $settings['countdown_intro'];
 		$date_time = strtotime($settings['date_time']);
-		$date = date_i18n("d F Y G:i", strtotime($settings['date_time']));
+		$date = date_i18n("d M Y G:i", strtotime($settings['date_time'])); ?>
 
-		$t = date("Y-m-d", $date_time);
-
-		?>
-
-        <div class="main-title df db-414 tac-414 justify-content-center">
-            <h2 class=""> <?php echo $title_text ?> </h2>
-            <div class="deal_countdown">
-			<!-- 2 may 2022 -->
-                <ul class="deal_counter bgc-thm" id="timer" data-endtime="<?php echo $t ?>"></ul>
-            </div>
-        </div>
+		<div class="countdown-wrap">
+			<div class="deal_countdown">
+				<ul class="deal_counter bgc-thm" id="timer" data-endtime="<?php echo esc_html($date); ?>">
+					<?php if( ! empty($countdown_intro) ) { ?>
+						<li class="list-inline-item"><span class="title"><?php echo esc_html($countdown_intro); ?>:</span></li>
+					<?php } ?>
+					<li class="list-inline-item days"></li>
+					<li class="list-inline-item"><span class="seperator">:</span></li>
+					<li class="list-inline-item hours"></li>
+					<li class="list-inline-item"><span class="seperator">:</span></li>
+					<li class="list-inline-item minutes"></li>
+					<li class="list-inline-item"><span class="seperator">:</span></li>
+					<li class="list-inline-item seconds"></li>
+				</ul>
+			</div>
+		</div>
 
 		<?php 
     }
--- assets/js/wpew-front.js
+++ assets/js/wpew-front.js
@@ -153,75 +153,37 @@ jQuery(document).ready(function($){
         $newWord.removeClass('is-hidden').addClass('is-visible');
     }
 
-
-    
-    // CountDown
-    function myTimer() {
-        var ending = jQuery("#timer").attr("data-endtime"),
-            endTime = new Date(ending);
-        endTime = Date.parse(endTime) / 1000;
-      
+    /**
+     * Countdown Script
+    **/
+    function makeTimer() {
+        //  var endTime = new Date("20 Dec 2022 9:56:00 GMT+01:00");  
+        var ending = jQuery("#timer").attr("data-endtime");
+        var endTime = new Date(ending);      
+        endTime = (Date.parse(endTime) / 1000);
         var now = new Date();
-        now = Date.parse(now) / 1000;
-      
+        now = (Date.parse(now) / 1000);
         var timeLeft = endTime - now;
-      
-        var days = Math.floor(timeLeft / 86400);
-        var hours = Math.floor((timeLeft - days * 86400) / 3600);
-        var minutes = Math.floor((timeLeft - days * 86400 - hours * 3600) / 60);
-        var seconds = Math.floor(
-          timeLeft - days * 86400 - hours * 3600 - minutes * 60
-        );
-      
-        if (days < "10") {
-          days = "0" + days;
-        }
-        if (days < "1") {
-          days = "0";
-        }
-        if (hours < "10") {
-          hours = "0" + hours;
-        }
-        if (hours < "1") {
-          hours = "0";
-        }
-        if (minutes < "10") {
-          minutes = "0" + minutes;
-        }
-        if (minutes < "1") {
-          minutes = "0";
-        }
-        if (seconds < "10") {
-          seconds = "0" + seconds;
-        }
-        if (seconds < "1") {
-          seconds = "0";
-        }
-      
-        $("#timer").html(
-            "<li class='list-inline-item'><span class='title'>Ends in:</span></li>"+
-            "<li class='list-inline-item hours'>" +
-            days +
-            "</li><li class='list-inline-item'><span class='seperator'>:</span></li>" +
-            "<li class='list-inline-item hours'>" +
-            hours +
-            "</li><li class='list-inline-item'><span class='seperator'>:</span></li>" +
-            "<li class='list-inline-item minutes'>" +
-            minutes +
-            "</li><li class='list-inline-item'><span class='seperator'>:</span></li>" +
-            "<li class='list-inline-item seconds'>" +
-            seconds +
-            "</li>"
-        );
+        var days = Math.floor(timeLeft / 86400); 
+        var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+        var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+        var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));  
+        if(days < 0) { days = 0 }
+        if (hours < "10") { hours = "0" + hours; }
+        if (minutes < "10") { minutes = "0" + minutes; }
+        if (seconds < "10") { seconds = "0" + seconds; }
+        $(".days").html(days + "<span> Days</span>");
+        $(".hours").html(hours + "<span> Hours</span>");
+        $(".minutes").html(minutes + "<span> Min</span>");
+        $(".seconds").html(seconds + "<span> Sec</span>");
     }
-      
-    setInterval(function() {
-        myTimer();
-    }, 1000);
-      
+    setInterval(function() { makeTimer(); }, 1000);
       
   
 });
+
+
+
 
 
 
